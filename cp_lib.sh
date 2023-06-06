0428f7df6dfb56342a7d071d75bc8f8534370419
try876 <small_tank@qq.com>
2023-06-06 06:49:05 -0400

    增加自动复制lib的脚本

---
+++ cp_lib.sh
@@ -0,0 +1,3 @@
+cp td/unix.x86_64/thosttraderapi_se.so /lib/libthosttraderapi_se.so
+cp data_collect/unix.x86_64/LinuxDataCollect.so /lib/libLinuxDataCollect.so
+cp md/unix.x86_64/thostmduserapi_se.so /lib/libthostmduserapi_se.so 
